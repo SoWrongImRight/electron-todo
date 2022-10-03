baad3a116075fe7011eb3ecc8992d87192bf4c2a
Russ Carroll <info@rcarroll.dev>
2022-10-03 14:20:24 -0400

    Addidn dev tool functionality.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const { app, BrowserWindow,Menu } = electron;
 
 let mainWindow;
+let addWindow
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ webPreferences: {
@@ -10,16 +11,29 @@ app.on('ready', () => {
   }})
 
   mainWindow.loadURL(`file:${__dirname}/main.html`)
+  mainWindow.on('closed', () => { app.quit() })
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
-})
+});
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`)
+};
 
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
-      { label: 'New Todo' },
+      { 
+        label: 'New Todo',
+        click() { createAddWindow() }
+      },
       { 
         label: 'Quit',
         accelerator: (() => {
@@ -40,3 +54,18 @@ const menuTemplate = [
 if (process.platform === 'darwin') {
   menuTemplate.unshift({});
 };
+
+if (process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'Developer',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Command+Alt+i' : 'Ctrl+Shift+i',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  })
+}
